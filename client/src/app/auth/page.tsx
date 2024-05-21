0ebe7f01b23ee223d333992c1cbe1d7a060d9832
ELFrancisco4 <offiongfrancis14@gmail.com>
2024-05-21 09:27:38 +0100

    Set up auth page and components

--- client/src/app/auth/page.tsx
+++ client/src/app/auth/page.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
-type Props = {}
+import React from "react";
+import Header from "@/components/Home/Header";
+import Navbar from "@/components/Home/Navbar";
+import Tabs from "@/components/Home/Tabs";
+import Footer from "@/components/Home/Footer";
+import AuthTabs from "@/components/Auth/authtabs";
 
-const page = (props: Props) => {
-  return ( <div> page </div> )
-}
+const page = () => {
+  return (
+    <>
+      <title>Login | Register</title>
+      <div>
+        <section className="text-white bg-gradient-to-t from-gray-800 to-gray-900 w-full h-fit">
+          <Header />
+          <Navbar />
+          <Tabs />
+        </section>
+        <AuthTabs/>
+        <Footer/>
+      </div>
+    </>
+  );
+};
 
-export default page
+export default page;
